e2aa5dd13b89c9b40859bb8d7f90d0d8140ea80f
Temi Lajumoke <temi@temilajumoke.com>
2017-11-12 22:51:40 +0100

    [chore] add test suites

---
+++ test/index.test.js
@@ -0,0 +1,33 @@
+var expect = require('chai').expect;
+var objectKeyMirror = require('../index');
+
+
+describe('objectKeyMirror', function() {
+  it('should return an object when an array supplied', function() {
+      expect(objectKeyMirror(['LOGIN_SUCCESS']))
+        .to.be.an('object');
+  });
+
+  it('should create an object with keys and properties of array supplied', 
+    function() {
+      expect(objectKeyMirror(['LOGIN_SUCCESS', 'SIGNUP_SUCCESS', 'LOGIN_FAILURE']))
+        .to.deep.equal({ LOGIN_SUCCESS: 'LOGIN_SUCCESS',SIGNUP_SUCCESS: 'SIGNUP_SUCCESS', LOGIN_FAILURE: 'LOGIN_FAILURE' });
+
+      expect(objectKeyMirror(['LOGIN_SUCCESS'])).to.deep
+      .equal({ LOGIN_SUCCESS: 'LOGIN_SUCCESS' });
+
+      expect(objectKeyMirror([1, 2, 3, 4])).to.deep
+        .equal({ 1: '1', 2: '2', 3: '3', 4: '4' });
+      
+      expect(objectKeyMirror(['john', 'doe', 'amos', 'smith'])).to.deep
+        .equal({ john: 'john', doe: 'doe', amos: 'amos', smith: 'smith' });
+
+      expect(objectKeyMirror(['john smith', 'doe', 'philip', 'smith'])).to.deep
+        .equal({ 
+          "john smith": 'john smith', 
+          doe: 'doe', 
+          philip: 'philip', 
+          smith: 'smith' 
+        });
+  });
+});
